d086eb1ef909312f4a65230ca746f6ce8cfdb3c4
nixon-tg <elotoja56@gmail.com>
2026-08-02 16:04:36 +0200

    Delete app.js

--- app.js
+++
@@ -1,6 +0,0 @@
-const tg=window.Telegram?.WebApp;
-if(tg){tg.ready();tg.expand();}
-function openMenu(){console.log("MENU");}
-function openContact(){console.log("KONTAKT");}
-document.getElementById("menuBtn").onclick=openMenu;
-document.getElementById("contactBtn").onclick=openContact;
